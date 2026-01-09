44a77706eb868ef69f6147be12a45af6d4663a83
Jose Castro <josetutis@gmail.com>
2026-01-09 08:58:47 +0100

    finished the project in scrimba about react routing

--- api.js
+++ api.js
@@ -1,6 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { 
+    getFirestore,
+     collection,
+     getDocs,
+     doc,
+     getDoc,
+     query,
+     where,
+
+ } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -32,7 +41,28 @@ export async function getVans() {
     return vans
 }
 
-//     const url = id ? `/api/vans/${id}` : "/api/vans"
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    const van = {...snapshot.data(),
+        id:snapshot.id
+    }
+    return van
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
 //     const res = await fetch(url)
 //     if (!res.ok) {
 //         throw {
@@ -44,20 +74,6 @@ export async function getVans() {
 //     const data = await res.json()
 //     return data.vans
 // }
-
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
