2f8470e97acb06b41d684cd517e467daee5ac6a1
Jose Castro <josetutis@gmail.com>
2026-01-09 08:34:30 +0100

    installed and connect firabase to my project

--- api.js
+++ api.js
@@ -1,16 +1,49 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3tskOF5aO4lBNujq7BCtDpCpJ6i4IXSI",
+  authDomain: "vanlife-53fb6.firebaseapp.com",
+  projectId: "vanlife-53fb6",
+  storageBucket: "vanlife-53fb6.firebasestorage.app",
+  messagingSenderId: "611144461444",
+  appId: "1:611144461444:web:1d384632cf4968bf2ce6d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+// Refactirong the fetching functions below
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id:doc.id
+    }))
+    return vans
 }
+
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
